605011a69649d1b132d5b83c8d73af7d744789f1
Jonathan Straub <j.f.straub1403@gmail.com>
2019-05-22 14:52:39 +0200

    Added text cleanup (remove empty lines & slides; replace '--' with '---') before reformatting

--- Reformatter/main.py
+++ Reformatter/main.py
@@ -7,38 +7,33 @@ import magic #Get Encoding of File
 argparser = argparse.ArgumentParser(description='Reformat *.sng files to the standard Immanuel format.')
 argparser.add_argument('files', nargs='+', help='Files to be reformatted. If directory, all *.sng files in it will be reformatted recursively.')
 
-#Regular Expressions
-lineStartToUpper = r'^\s*(#\d)?\s*([a-zäöü])'
-lineEndRmSymbols = r'\W$'
-apostroph = r'[\'`]'
+def cleanup(text):
+    result = text
+    result = re.sub(r'\n\s*\n', r'\n', result) #Remove empty lines
+    result = re.sub(r'\n--\n', r'\n---\n', result) #Fix only 2 dashes for slide separation
+    result = re.sub(r'\n---\n---\n', r'\n---\n', result) #Remove empty slides
+    return result
 
 def lineStartUppercase(matchobj):
     return matchobj.group(2).upper()
 
 def formatLine(line):
     #Make character at line start uppercase
-    line = re.sub(lineStartToUpper, lineStartUppercase, line)
-    line = re.sub(lineEndRmSymbols, '', line)
-    line = re.sub(apostroph, '\'', line)
+    line = re.sub(r'^\s*(#\d)?\s*([a-zäöü])', lambda mobj: mobj.group(2).upper(), line) #Line start to upper case
+    line = re.sub(r'[^\w-]$', r'', line) #Line end remove Symbols
+    line = re.sub(r'[\'`]', r'\'', line) #Replace accents with apostrophs
     return line
-
-def cleanup(text):
-    
-    pass
 
 def format(text):
     inStart = True
+    text = cleanup(text)
     result = ''
     for line in text.splitlines():
         if inStart and not line == '---':
             result += line + '\n'
             continue
         inStart = False
-        if line == '':
-            continue
         result += formatLine(line) + '\n'
-        pass
-    result = cleanup(result)
     return result
 
 def determineCharset(filename):
@@ -52,7 +47,6 @@ def parse(filename):
     elif os.path.isfile(filename):
         with open(filename, 'r+', encoding=determineCharset(filename)) as f:
             print(format(f.read()))
-            pass
     else:
         raise FileNotFoundError
 
