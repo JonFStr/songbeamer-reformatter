6143469731e79ee7c23aff288c654e08d83b978a
Alexander Hornig <alexander@h-software.de>
2020-12-09 17:48:36 +0000

    Fixed comments style

--- main.py
+++ main.py
@@ -6,27 +6,29 @@ import chardet
 # Parsing CMD arguments
 argparser = argparse.ArgumentParser(
     description='Reformat *.sng files to the standard Immanuel format.')
-# input files
+# Input files
 argparser.add_argument(
     'files', nargs='+',
     help='Files to be reformatted. If directory,\
     all *.sng files in it will be reformatted recursively.')
-# output directory
+# Output directory
 argparser.add_argument(
     '-o', '--output-directory', dest='out',
     help='Write files in this output directory \
     instead of overwriting input files. Keeps directory structure')
-# parse the args now to avoid unneeded execution of code
+# Parse the args now to avoid unneeded execution of code
 args = argparser.parse_args()
 
 
 def cleanup(text):
     result = text
-    result = re.sub(r'\n\s*\n', r'\n', result)  # Remove empty lines
+    # Remove empty lines
+    result = re.sub(r'\n\s*\n', r'\n', result)
     # Fix only 2 dashes for slide separation
     result = re.sub(r'\n--\n', r'\n---\n', result)
-    result = re.sub(r'\n---\n---\n', r'\n---\n', result)  # Remove empty slides
-    # remove slide seperator at eof
+    # Remove empty slides
+    result = re.sub(r'\n---\n---\n', r'\n---\n', result)
+    # Remove slide seperator at eof
     result = re.sub(r'\n---\n$', r'\n', result)
     return result
 
@@ -38,12 +40,13 @@ def formatLine(line):
                   lambda mobj: mobj.group(2).upper(), line)
     # Line end remove Symbols except quotes and dashes
     line = re.sub(r'[^\w\-"]$', r'', line)
-    line = re.sub(r'[\'`´]', r'\'', line)  # Replace accents with apostrophes
+    # Replace accents with apostrophes
+    line = re.sub(r'[\'`´]', r'\'', line)
     return line
 
 
 def format(text):
-    # we are in the file header
+    # We are in the file header
     inStart = True
 
     text = cleanup(text)
@@ -53,32 +56,37 @@ def format(text):
         # FontSize is apparently used outside the header
         if not inStart and '#FontSize=' in line:
             continue
-        if inStart and not line == '---':  # Don't change header
+        # Don't change header
+        if inStart and not line == '---':
             result += line + '\n'
             continue
-        inStart = False  # With the first '---', we are out of the header
+        # With the first '---', we are out of the header
+        inStart = False
         result += formatLine(line) + '\n'
     return result
 
 
 def parse(filename, outdir):
-    if os.path.isdir(filename):  # if directory, parse subcontents recursively
-        # if out is given and the directory does not exist in outdir, create it
+    # If directory, parse subcontents recursively
+    if os.path.isdir(filename):
+        # If out is given and the directory does not exist in outdir, create it
         if outdir and not os.path.isdir(os.path.join(outdir, filename)):
             os.makedirs(os.path.join(outdir, filename))
-        # recursively call parse for subdirectories
+        # Recursively call parse for subdirectories
         for subfile in os.listdir(filename):
-            # appending current filename -> don't change the working directory
+            # Appending current filename -> don't change the working directory
             parse(filename + '/' + subfile, outdir)
     elif os.path.isfile(filename):
         raw = None
-        with open(filename, 'rb') as infile:  # open binary -> encoding later
+        # Open binary -> encoding later
+        with open(filename, 'rb') as infile:
             raw = infile.read()
-        # format decoded string (prevents empty files on error)
+        # Format decoded string (prevents empty files on error)
         formatted = format(raw.decode(chardet.detect(raw)['encoding']))
-        # open file for writin (if necessary with outdir)
+        # Open file for writin (if necessary with outdir)
         with open(os.path.join(outdir, filename), 'w') as outfile:
-            outfile.write(formatted)  # write out
+            # Write out
+            outfile.write(formatted)
     else:
         raise FileNotFoundError
 
